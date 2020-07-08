301e332482a1c5ec4d292e62c709793788db3473
kkho5555 <kkho5555@gmail.com>
2020-07-08 14:19:30 +0900

    frontend setup

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { ApolloProvider } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
-import Client from "../Apollo/Client";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <ApolloProvider client={Client}>
-      <GlobalStyles />
-      <AppRouter isLoggedIn={false}></AppRouter>
-    </ApolloProvider>
-  </ThemeProvider>
-);
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+export default () => {
+  const {
+    data: { isLoggedIn },
+  } = useQuery(QUERY);
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
+      </>
+    </ThemeProvider>
+  );
+};
