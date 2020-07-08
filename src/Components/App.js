658c94767d56e7dedd10d0d18b3634758095ee33
kkho5555 <kkho5555@gmail.com>
2020-07-08 09:48:16 +0900

    second commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
+import AppRouter from "./Router";
 
 export default () => (
   <ThemeProvider theme={Theme}>
     <GlobalStyles />
-    안녕하세요?
-    <div>ddsf</div>
+    <AppRouter isLoggedIn={false}></AppRouter>
   </ThemeProvider>
 );
---
+++ src/Routes/Post.js
@@ -0,0 +1,1 @@
+export default () => "Show Someone's Post";
